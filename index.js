047bce6c3766b1a68da2edfdb29aa50921e5e317
Bravelike <108171366+Bravelike@users.noreply.github.com>
2024-12-15 18:41:16 +0100

    main

--- index.js
+++ index.js
@@ -1,41 +1,8 @@
-// const navigation = document.getElementById("hamburger");
-// const navigationX = document.getElementById("cancel");
-
-// navigation.addEventListener('click', function () {
-//     if (navigation.classList.contains('dropdown-menu')) {
-//         navigation.classList.remove('dropdown-menu');
-//     } else {
-//         navigation.classList.add('dropdown-menu');
-//     }
-// });
-
-// navigationX.addEventListener('click', function () {
-//     if (navigation.classList.contains('dropdown-menu')) {
-//         navigation.classList.remove('dropdown-menu');
-//     } else {
-//         navigation.classList.add('dropdown-menu');
-//     }
-// });
-const navigation = document.getElementById("hamburger");
-const navigationX = document.getElementById("cancel");
-
-// (function(params){
-//     params.addEventListener('click', function(){
-//         if(navigation.classList.contains('dropdown-menu')){
-//             navigation.classList.remove('dropdown-menu');
-//             navigation.classList.add('.dropdown-menu')
-//         }else{
-//             navigation.classList.add('dropdown-menu')
-//         }
-//     })
-// })(navigation)
-(function(params){
-    params.addEventListener('click', function(){
-        if(navigationX.classList.contains('dropdown-menu')){
-            navigationX.classList.remove('dropdown-menu');
-            navigationX.classList.add('.dropdown-menu')
-        }else{
-            navigationX.classList.add('dropdown-menu')
-        }
-    })
-})(navigationX)
+function showsidebar() {
+    const sidebar = document.querySelector('.side-bar');
+    sidebar.style.display = "flex"
+}
+function hidesidebar()  {
+    const sidebar = document.querySelector('.side-bar');
+    sidebar.style.display = "none"
+}
